1dc6ecae1dfe4932a67fe6757981ef266e8bb32f
ZoeTheDaw <zoedaw17@gmail.com>
2024-11-24 19:21:50 +0800

    Create HHParticleRegistry.java

---
+++ src/main/java/com/internals/halcyonhorizons/particle/HHParticleRegistry.java
@@ -0,0 +1,12 @@
+package com.internals.halcyronhorizons.particle;
+
+import com.internals.halcyonhorizons.HalcyonHorizons;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
