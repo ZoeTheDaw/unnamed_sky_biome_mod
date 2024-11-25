4d3ff6ed0c4a7bcefecd9d4fe9b8d6e1d209935e
ZoeTheDaw <zoedaw17@gmail.com>
2024-11-25 22:03:35 +0800

    Update HalcyonHorizons.java

--- src/main/java/com/internals/halcyonhorizons/HalcyonHorizons.java
+++ src/main/java/com/internals/halcyonhorizons/HalcyonHorizons.java
@@ -1,4 +1,8 @@
 package com.internals.halcyonhorizons;
 
-import com.internals.halcyonhorizons.server.entity.HorizonsEntitygistry;
+import com.internals.halcyonhorizons.server.particle.HorizonParticleRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsBlockRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsEntityRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsInventoryRegistry;
+
 
