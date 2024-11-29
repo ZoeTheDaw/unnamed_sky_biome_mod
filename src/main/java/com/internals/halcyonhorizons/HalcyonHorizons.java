f98ff1a277ac524be4d6ae2e2aa65fd88d54c852
ZoeTheDaw <zoedaw17@gmail.com>
2024-11-29 09:39:35 +0800

    Update HalcyonHorizons.java
    Put in more of the registries

--- src/main/java/com/internals/halcyonhorizons/HalcyonHorizons.java
+++ src/main/java/com/internals/halcyonhorizons/HalcyonHorizons.java
@@ -1,8 +1,13 @@
 package com.internals.halcyonhorizons;
 
-import com.internals.halcyonhorizons.server.particle.HorizonParticleRegistry;
+import com.internals.halcyonhorizons.client.particle.HorizonParticleRegistry;
 import com.internals.halcyonhorizons.server.entity.HorizonsBlockRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsEnchantmentRegistry;
 import com.internals.halcyonhorizons.server.entity.HorizonsEntityRegistry;
 import com.internals.halcyonhorizons.server.entity.HorizonsInventoryRegistry;
-
+import com.internals.halcyonhorizons.server.entity.HorizonsItemRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsLevelRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsMessageRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsPotionyRegistry;
+import com.internals.halcyonhorizons.server.entity.HorizonsRecipeyRegistry;
 
