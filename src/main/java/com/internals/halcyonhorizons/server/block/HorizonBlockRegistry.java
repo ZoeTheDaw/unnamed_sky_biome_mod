24d0b1b2c9f8d36d37e693f497831094fff00c04
ZoeTheDaw <zoedaw17@gmail.com>
2024-11-30 16:21:29 +0800

    Added fluff pulp in the registry

---
+++ src/main/java/com/internals/halcyonhorizons/server/block/HorizonBlockRegistry.java
@@ -0,0 +1,23 @@
+import com.github.alexmodguy.alexscaves.server.item.*;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class HorizonsBlockRegistry {
+    public final static RegistryObject<Block> Fluffpulp = registerBlockAndItem("fluffpulp", () -> new Block());
+}
