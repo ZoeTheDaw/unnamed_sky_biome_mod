f6201253cc2a3190c6361e4ca66077e884858ab7
ZoeTheDaw <zoedaw17@gmail.com>
2024-12-01 19:50:27 +0800

    Setting up the bases for generatoon structures

---
+++ src/main/java/com/internals/halcyonhorizons/server/level/structure/InfirmaryFluff.java
@@ -0,0 +1,10 @@
+package com.internals.halcyonhorizons.server.level.structure;
+
+import com.internals.halcyonhorizons.server.level.biome.HorizonsBiomeRegistry;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.WorldgenRandom;
+import net.minecraft.world.level.levelgen.structure.StructurePiece;
+import net.minecraft.world.level.levelgen.structure.StructureType;
+
